f500454b5f3408093f6e9242ae1a85dde27a5270
Thai <tranthai.070104@gmail.com>
2024-08-19 13:41:33 +0700

    express Router

---
+++ src/routes/v1/boardRoutes.js
@@ -0,0 +1,20 @@
+/**
+ * Updated by trungquandev.com's author on August 17 2023
+ * YouTube: https://youtube.com/@trungquandev
+ * "A bit of fragrance clings to the hand that gives flowers!"
+ */
+import express from 'express'
+import { StatusCodes } from 'http-status-codes'
+
+
+const Router = express.Router()
+
+Router.route('/')
+    .get((req, res) => {
+        res.status(StatusCodes.OK).json({ message: 'API get list boards' })
+    })
+    .post((req, res) => {
+        res.status(StatusCodes.CREATED).json({ message: 'API craete new boards' })
+    })
+
+export const boardRoutes = Router
--- src/server.js
+++ src/server.js
@@ -8,23 +8,18 @@ import express from 'express'
 import exitHook from 'async-exit-hook'
 import { CONNECT_DB, ClOSE_DB } from './config/mongodb'
 import { env } from '~/config/environment'
+import { APIs_V1 } from './routes/v1'
 
 const START_SERVER = () => {
   const app = express()
 
-  const hostname = 'localhost'
-  const port = 8017
-
-  app.get('/', (req, res) => {
-    res.end('<h1>Hello World!</h1><hr>')
-  })
-
+  app.use('/v1', APIs_V1)
   app.listen(env.APP_PORT, env.APP_HOST, () => {
     // eslint-disable-next-line no-console
     console.log(`Hello ${env.AUTHOR}, I am running at http://${env.APP_HOST}:${env.APP_PORT}/`)
   })
   //Thực hiện các tác vụ cleanup trước khi dừng server
-  exitHook((signal) => {
+  exitHook(() => {
     ClOSE_DB()
 
   })
