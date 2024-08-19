042a82492d351f69135cdcc21b5c3b307bdf3321
Thai <tranthai.070104@gmail.com>
2024-08-19 11:09:18 +0700

    connect_mongoDB and env varible

--- src/config/mongodb.js
+++ src/config/mongodb.js
@@ -3,3 +3,38 @@
  * YouTube: https://youtube.com/@trungquandev
  * "A bit of fragrance clings to the hand that gives flowers!"
  */
+//thaidev
+//MPJ8fjhR5bD8r0wQ
+
+import { MongoClient, ServerApiVersion } from 'mongodb'
+import { env } from './environment'
+
+//Khởi tạo một đối tượng trelloDatabaseInstall ban đầu là null
+let trelloDatabaseInstall = null
+const mongoClientInstance = new MongoClient(env.MONGODB_URL, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true
+  }
+
+
+})
+
+//Kết nối tới Database
+export const CONNECT_DB = async () => {
+  //Gọi kết nối tới mongoDB Atlas với Url đã khai báo
+  await mongoClientInstance.connect()
+  //Kết nối thành công thì lấy
+  trelloDatabaseInstall = mongoClientInstance.db(env.DATABASE_NAME)
+}
+
+export const GET_DB = () => {
+  if (!trelloDatabaseInstall) throw new Error('Must connect to Database first!')
+  return trelloDatabaseInstall
+}
+
+//Đóng kết nối tới Database khi cần
+export const ClOSE_DB = async () => {
+  await mongoClientInstance.close()
+}
--- src/server.js
+++ src/server.js
@@ -5,28 +5,37 @@
  */
 
 import express from 'express'
-import { mapOrder } from '~/utils/sorts.js'
+import exitHook from 'async-exit-hook'
+import { CONNECT_DB, ClOSE_DB } from './config/mongodb'
+import { env } from '~/config/environment'
 
-const app = express()
+const START_SERVER = () => {
+  const app = express()
 
-const hostname = 'localhost'
-const port = 8017
+  const hostname = 'localhost'
+  const port = 8017
 
-app.get('/', (req, res) => {
-  // Test Absolute import mapOrder
-  console.log(mapOrder(
-    [ { id: 'id-1', name: 'One' },
-      { id: 'id-2', name: 'Two' },
-      { id: 'id-3', name: 'Three' },
-      { id: 'id-4', name: 'Four' },
-      { id: 'id-5', name: 'Five' } ],
-    ['id-5', 'id-4', 'id-2', 'id-3', 'id-1'],
-    'id'
-  ))
-  res.end('<h1>Hello World!</h1><hr>')
-})
+  app.get('/', (req, res) => {
+    res.end('<h1>Hello World!</h1><hr>')
+  })
 
-app.listen(port, hostname, () => {
-  // eslint-disable-next-line no-console
-  console.log(`Hello Trung Quan Dev, I am running at ${ hostname }:${ port }/`)
-})
+  app.listen(env.APP_PORT, env.APP_HOST, () => {
+    // eslint-disable-next-line no-console
+    console.log(`Hello ${env.AUTHOR}, I am running at http://${env.APP_HOST}:${env.APP_PORT}/`)
+  })
+  //Thực hiện các tác vụ cleanup trước khi dừng server
+  exitHook((signal) => {
+    ClOSE_DB()
+
+  })
+}
+
+CONNECT_DB()
+  .then(() => console.log('Connected to MongoDB Cloud Atlas'))
+  .then(() => START_SERVER())
+  .catch(error => {
+    console.error(error)
+    process.exit(0)
+  })
+
+
