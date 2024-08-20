7b2fc00d3c04746de67d46bdff9f09e940cc865b
Thai <tranthai.070104@gmail.com>
2024-08-20 07:49:10 +0700

    Service

--- src/controllers/boardController.js
+++ src/controllers/boardController.js
@@ -5,12 +5,15 @@
  */
 
 import { StatusCodes } from 'http-status-codes'
+import { boardService } from '~/services/boardService'
+
 
 const createNew = async (req, res, next) => {
     try {
-
+        //Điều hướng dữ liệu sang tầng Service
+        const createdBoard = await boardService.createNew(req.body)
         // Có kết quả thì trả về phía Client
-        res.status(StatusCodes.CREATED).json({ message: 'POST from Controller:API craete new boards' })
+        res.status(StatusCodes.CREATED).json(createdBoard)
     } catch (error) { next(error) }
 }
 
