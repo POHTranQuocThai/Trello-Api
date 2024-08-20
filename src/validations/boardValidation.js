6b1830e96c48b469dd541baa9e6b9c1398a82c84
Thai <tranthai.070104@gmail.com>
2024-08-20 20:44:52 +0700

    mongodb aggregate

--- src/validations/boardValidation.js
+++ src/validations/boardValidation.js
@@ -6,7 +6,7 @@
 import Joi from 'joi'
 import { StatusCodes } from 'http-status-codes'
 import ApiError from '~/utils/ApiError'
-
+import { BOARD_TYPES } from '~/utils/constants'
 const createNew = async (req, res, next) => {
   /**
      * Note: Mặc định chúng ta không cần phải custom message ở phía BE làm gì vì để FE tự validate và custom phía FE cho đẹp
@@ -16,7 +16,8 @@ const createNew = async (req, res, next) => {
     */
   const correctCondition = Joi.object({
     title: Joi.string().required().min(3).max(50).trim().strict(),
-    description: Joi.string().required().min(3).max(256).trim().strict()
+    description: Joi.string().required().min(3).max(256).trim().strict(),
+    type: Joi.string().valid(BOARD_TYPES.PUBLIC, BOARD_TYPES.PRIVATE).required()
   })
   try {
     //Chỉ định abortEarly: false để trường hợp có nhiều lỗi validation thì trả về tất cả lỗi
