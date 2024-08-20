0c01dacef10edd20b2cd9e49f326db3ccaf1da6e
Thai <tranthai.070104@gmail.com>
2024-08-20 09:40:04 +0700

    model schema

--- src/models/boardModel.js
+++ src/models/boardModel.js
@@ -4,6 +4,7 @@
  * "A bit of fragrance clings to the hand that gives flowers!"
  */
 import Joi from 'joi'
+import { ObjectId } from 'mongodb'
 import { GET_DB } from '~/config/mongodb'
 import { OBJECT_ID_RULE, OBJECT_ID_RULE_MESSAGE } from '~/utils/validators'
 
@@ -17,9 +18,13 @@ const BOARD_COLLECTION_SCHEMA = Joi.object({
     updatedAt: Joi.date().timestamp('javascript').default(null),
     _destroy: Joi.boolean().default(false)
 })
+const validateBeforeCreate = async (data) => {
+    return await BOARD_COLLECTION_SCHEMA.validateAsync(data, { abortEarly: false })
+}
 const createNew = async (data) => {
     try {
-        const createdBoard = await GET_DB().collection(BOARD_COLLECTION_NAME).insertOne(data)
+        const validData = await validateBeforeCreate(data)
+        const createdBoard = await GET_DB().collection(BOARD_COLLECTION_NAME).insertOne(validData)
         return createdBoard
     } catch (error) { throw new Error(error) }
 
@@ -27,9 +32,7 @@ const createNew = async (data) => {
 
 const findOneById = async (id) => {
     try {
-        const result = await GET_DB().collection(BOARD_COLLECTION_NAME).findOne({
-            _id: id
-        })
+        const result = await GET_DB().collection(BOARD_COLLECTION_NAME).findOne({ _id: new ObjectId(id) })
         return result
     } catch (error) { throw new Error(error) }
 }
