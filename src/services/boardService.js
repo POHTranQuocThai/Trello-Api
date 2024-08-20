510bd62e6e74a836379f70efcfb001c955750e26
Thai <tranthai.070104@gmail.com>
2024-08-20 09:12:50 +0700

    Api create

--- src/services/boardService.js
+++ src/services/boardService.js
@@ -6,6 +6,7 @@
  * "A bit of fragrance clings to the hand that gives flowers!"
  */
 
+import { boardModel } from '~/models/boardModel'
 import slugify from '~/utils/formatters'
 
 const createNew = async (reqBody) => {
@@ -18,11 +19,12 @@ const createNew = async (reqBody) => {
         }
 
         //Gọi tới tầng Model để xử lý lưu bản ghi newBoard vào trong Database
+        const createdBoard = await boardModel.createNew(newBoard)
         //Làm thêm các xử lý logic khác với các collection khác tùy đặc thù dự án
         //Bắn email, notification về cho admin khi có 1 cái board mới được tạo
 
         //Trả kết quả về, trong service luôn phải có return
-        return newBoard
+        return createdBoard
     } catch (error) { throw error }
 }
 
