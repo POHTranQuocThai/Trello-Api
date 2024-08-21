5c4d86346dd0ee522ff29f2b604b3bdf74605349
Thai <tranthai.070104@gmail.com>
2024-08-21 18:22:47 +0700

    get API p1

---
+++ src/services/cardService.js
@@ -0,0 +1,27 @@
+
+import { cardModel } from '~/models/cardModel'
+
+const createNew = async (reqBody) => {
+    // eslint-disable-next-line no-useless-catch
+    try {
+        //Xử lý logic dữ liệu tùy đặc thù dự án
+        const newCard = {
+            ...reqBody
+        }
+
+        //Gọi tới tầng Model để xử lý lưu bản ghi newCard vào trong Database
+        const createdCard = await cardModel.createNew(newCard)
+        //Lấy bản ghi card sau khi gọi
+        const getNewCard = await cardModel.findOneById(createdCard.insertedId)
+        //Làm thêm các xử lý logic khác với các collection khác tùy đặc thù dự án
+        //Bắn email, notification về cho admin khi có 1 cái card mới được tạo
+
+        //Trả kết quả về, trong service luôn phải có return
+        return getNewCard
+    } catch (error) { throw error }
+}
+
+export const cardService = {
+    createNew
+
+}
