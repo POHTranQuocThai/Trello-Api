aabd6da802602430eaa85d3f430e97c0451ea17c
Thai <tranthai.070104@gmail.com>
2024-08-25 12:46:39 +0700

    deploy prod

--- src/config/environment.js
+++ src/config/environment.js
@@ -8,8 +8,8 @@ import 'dotenv/config'
 export const env = {
   MONGODB_URL: process.env.MONGODB_URL,
   DATABASE_NAME: process.env.DATABASE_NAME,
-  APP_HOST: process.env.APP_HOST,
-  APP_PORT: process.env.APP_PORT,
+  LOCAL_DEV_APP_HOST: process.env.LOCAL_DEV_APP_HOST,
+  LOCAL_DEV_APP_PORT: process.env.LOCAL_DEV_APP_PORT,
 
   BUILD_MODE: process.env.BUILD_MODE,
 
