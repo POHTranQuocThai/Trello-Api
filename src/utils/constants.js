b2ed7e5dda2c5793db10f2edf837328d1aacf34b
POHTranQuocThai <tranthai.070104@gmail.com>
2024-10-22 21:48:52 +0700

    Fix CORS policy

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -6,7 +6,7 @@
 
 export const WHITELIST_DOMAINS = [
     // 'http://localhost:5173' //Không cần localhost nữa vì ở file config/cors đã luôn cho phép môi trường dev (env.BUILD_MODE === 'dev)
-    'https://trello-web-blue-delta.vercel.app'
+    'https://trello-5bn5lsue9-pohs-projects-4dc4ae92.vercel.app'
 ]
 
 export const BOARD_TYPES = {
