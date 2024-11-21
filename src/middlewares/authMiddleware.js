472097fe51678055515331a09263ac1b5e2f913b
POHTranQuocThai <tranthai.070104@gmail.com>
2024-11-21 21:50:58 +0700

    HttpOnly Cookie ,AuthMiddleware, Redux Persist, protected Routes

---
+++ src/middlewares/authMiddleware.js
@@ -0,0 +1,39 @@
+import { StatusCodes } from "http-status-codes"
+import { env } from "~/config/environment"
+import { JwtProvider } from "~/providers/JwtProvider"
+import ApiError from "~/utils/ApiError"
+
+
+const isAuthorized = async (req, res, next) => {
+    //Lấy access token phía request cookies phía client - trong file authorizedAxios
+    const clientAccessToken = req.cookies?.accessToken
+    //Nếu không tồn tại
+    if (!clientAccessToken) {
+        next(new ApiError(StatusCodes.UNAUTHORIZED, 'Unauthorized! token not found!'))
+        return
+    }
+
+    try {
+        //b1 Thực hiện giải mã token xem nó có hợp lệ không
+        const accessTokenDecoded = await JwtProvider.verifyToken(clientAccessToken, env.ACCESS_TOKEN_SECRET_SIGNATURE)
+        //b2 Quan trọng: Nếu như cái token hợp lệ, cần phải lưu thông tin giải mã được vào cái req
+        req.jwtDecoded = accessTokenDecoded
+        //b3 Cho phép cái request đi tiếp
+        next()
+    } catch (error) {
+        //Nếu accessToken hết hạn thì mình cần trả về một cái lỗi GONE -401 cho phía FE để gọi api refreshToken
+        if (error?.message?.includes('jwt exprired')) {
+            next(new ApiError(StatusCodes.GONE, 'Need to refresh token!'))
+            return
+        }
+
+        //Nếu cái accessToken đó không hợp lệ do bất kỳ điều gì ngoài hét hạn thì cử trả về lỗi 401 cho FE goi api sign_out
+        next(new ApiError(StatusCodes.UNAUTHORIZED, 'Unauthorized'))
+    }
+
+
+}
+
+export const authMiddleware = {
+    isAuthorized
+}
--- src/routes/v1/boardRoute.js
+++ src/routes/v1/boardRoute.js
@@ -7,18 +7,19 @@ import express from 'express'
 import { StatusCodes } from 'http-status-codes'
 import { boardValidation } from '~/validations/boardValidation'
 import { boardController } from '~/controllers/boardController'
+import { authMiddleware } from '~/middlewares/authMiddleware'
 const Router = express.Router()
 
 Router.route('/')
-  .get((req, res) => {
+  .get(authMiddleware.isAuthorized, (req, res) => {
     res.status(StatusCodes.OK).json({ message: 'API get list boards' })
   })
-  .post(boardValidation.createNew, boardController.createNew)
+  .post(authMiddleware.isAuthorized, boardValidation.createNew, boardController.createNew)
 Router.route('/:id')
-  .get(boardController.getDetails)
-  .put(boardValidation.update, boardController.update)
+  .get(authMiddleware.isAuthorized, boardController.getDetails)
+  .put(authMiddleware.isAuthorized, boardValidation.update, boardController.update)
 //API hỗ trợ việc di chuyển card giữa các column khác nhau trong một board
 Router.route('/supports/moving_card')
-  .put(boardValidation.moveCardToDifferentColumn, boardController.moveCardToDifferentColumn)
+  .put(authMiddleware.isAuthorized, boardValidation.moveCardToDifferentColumn, boardController.moveCardToDifferentColumn)
 
 export const boardRoute = Router
