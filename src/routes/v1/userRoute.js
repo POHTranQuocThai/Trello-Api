87da8dd612c68381e1bb4adb8f022faca9488823
POHTranQuocThai <tranthai.070104@gmail.com>
2024-11-21 16:53:33 +0700

    finish verify acc jWT token

--- src/routes/v1/userRoute.js
+++ src/routes/v1/userRoute.js
@@ -5,4 +5,10 @@ const Router = express.Router()
 
 Router.route('/register')
     .post(userValidation.createNew, userController.createNew)
+
+Router.route('/verify')
+    .put(userValidation.verifyAccount, userController.verifyAccount)
+
+Router.route('/login')
+    .post(userValidation.login, userController.login)
 export const userRoute = Router
