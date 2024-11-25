bdf641ff78290b986a9e16087d9d33a2022bd32e
POHTranQuocThai <tranthai.070104@gmail.com>
2024-11-25 17:14:12 +0700

    Logout, refreshToken tự động với axios

--- src/routes/v1/userRoute.js
+++ src/routes/v1/userRoute.js
@@ -11,4 +11,8 @@ Router.route('/verify')
 
 Router.route('/login')
     .post(userValidation.login, userController.login)
+Router.route('/logout')
+    .delete(userController.logout)
+Router.route('/refresh_token')
+    .get(userController.refreshToken)
 export const userRoute = Router
