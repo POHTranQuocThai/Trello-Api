f73ef4bb69f37e26abcf220f8f72ecd7df395125
POHTranQuocThai <tranthai.070104@gmail.com>
2024-11-28 19:22:09 +0700

    Tính năng Create, Get Board Details, Update Column Title....vv

--- src/controllers/boardController.js
+++ src/controllers/boardController.js
@@ -5,17 +5,20 @@ import { boardService } from '~/services/boardService'
 
 const createNew = async (req, res, next) => {
   try {
+    const userId = req.jwtDecoded._id
+
     //Điều hướng dữ liệu sang tầng Service
-    const createdBoard = await boardService.createNew(req.body)
+    const createdBoard = await boardService.createNew(userId, req.body)
     // Có kết quả thì trả về phía Client
     res.status(StatusCodes.CREATED).json(createdBoard)
   } catch (error) { next(error) }
 }
 const getDetails = async (req, res, next) => {
   try {
+    const userId = req.jwtDecoded._id
     const boardId = req.params.id
     //Điều hướng dữ liệu sang tầng Service
-    const board = await boardService.getDetails(boardId)
+    const board = await boardService.getDetails(userId, boardId)
     // Có kết quả thì trả về phía Client
     res.status(StatusCodes.OK).json(board)
   } catch (error) { next(error) }
